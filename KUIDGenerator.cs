b5b34171b641eb37e4c6f7da83a3dac5d21cc32b
Sithura Myo <66086921+sisisama@users.noreply.github.com>
2023-08-04 22:21:11 +0630

    Add project files.

---
+++ KUIDGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace UniqueIdAlgorithms
+{
+    public class KUIDGenerator
+    {
+        private static readonly object LockObject = new object { };
+        private long lastTimestamp = -1;
+        private long sequence = 0;
+        private int uniqueIdCounter = 0; // Simple counter for demonstration
+
+        private const long Twepoch = 99999L;
+
+        public int GenerateKuid()
+        {
+            lock (LockObject)
+            {
+                long timestamp = GetCurrentTimestamp();
+                if (timestamp < lastTimestamp)
+                {
+                    throw new Exception("Clock moved backwards! Wait a moment.");
+                }
+                if (timestamp == lastTimestamp)
+                {
+                    sequence = (sequence + 1) & 4095;
+                }
+                if (sequence == 0)
+                {
+                    timestamp = WaitNextmillis(lastTimestamp);
+                }
+                else
+                {
+                    sequence = 0;
+                }
+                lastTimestamp = timestamp;
+
+                int kuid = (int)(((timestamp - Twepoch) << 22) | (sequence << 10) | GetUniqueId());
+
+                int positiveKuid = (kuid & 0x7FFFFFFF) % 90000 + 10000;
+                return positiveKuid;
+            }
+        }
+
+
+        public long GetCurrentTimestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+
+        public long WaitNextmillis(long lastTimestamp)
+        {
+            long timestamp = GetCurrentTimestamp();
+            while (timestamp <= lastTimestamp)
+            {
+                timestamp = GetCurrentTimestamp();
+            }
+            return timestamp;
+        }
+
+        private int GetUniqueId()
+        {
+            return uniqueIdCounter++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UniqueIdAlgorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            KUIDGenerator kuidGenerator = new KUIDGenerator();
+            for (int i = 0; i < 5; i++)
+            {
+                int uniqueId = kuidGenerator.GenerateKuid();
+                Console.WriteLine($"Generated Unique ID: STRM{uniqueId}");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
