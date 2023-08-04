02adc663e629948b4b22b41ca711c8182660da1e
Sithura Myo <66086921+sisisama@users.noreply.github.com>
2023-08-04 23:04:33 +0630

    [.]

--- KUIDGenerator.cs
+++ KUIDGenerator.cs
@@ -1,14 +1,28 @@
 ﻿using System;
-
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+using System.Xml.Linq;
 namespace UniqueIdAlgorithms
 {
     public class KUIDGenerator
     {
+        //LockObject line is defining a synchronization object (a lock) named LockObject.
+        //This object is used for thread synchronization to ensure that the critical section of code within the GenerateKuid() method is executed by only one thread at a time.
+        //This is important to maintain the consistency and correctness of the generated IDs when multiple threads are involved.
         private static readonly object LockObject = new object { };
-        private long lastTimestamp = -1;
-        private long sequence = 0;
-        private int uniqueIdCounter = 0; // Simple counter for demonstration
 
+        //lastTimestamp variable is used to track the most recent timestamp that was used for generating an ID.
+        //Its purpose is to ensure that IDs generated in the same millisecond are assigned unique values
+        //by utilizing the sequence component of the ID.
+        private long lastTimestamp = -1;
+
+        //sequence variable is used to prevent conflicts between multiple IDs generated within the same millisecond.
+        //It's a common technique in ID generation systems to include
+        //a sequence number to ensure that even if multiple IDs are generated in the same millisecond.
+        private long sequence = 0;
+
+        // Simple counter for demonstration
+        private int uniqueIdCounter = 0;
         private const long Twepoch = 99999L;
 
         public int GenerateKuid()
@@ -58,17 +72,32 @@ namespace UniqueIdAlgorithms
                 //This is likely done to ensure the value fits within the 32 - bit integer range.
                 int kuid = (int)(((timestamp - Twepoch) << 22) | (sequence << 10) | UniqueIdCounter());
 
+
+
                 //Format for 5 digit.U can remove or increase 0 to 90000 + 10000 (In this case u need to plus 100000 because
+
+
                 //it's only 900000 u can get 5 digit key or 5 digit key).
-                int positiveKuid = (kuid & 0x7FFFFFFF) % 90000 + 10000;
-                return positiveKuid;
+                int Kuid = (kuid & 0x7FFFFFFF) % 90000 + 10000;
+
+                //it's only 900000 u can get 5 digit key or 6 digit key).
+                //int Kuid = (kuid & 0x7FFFFFFF) % 900000 + 100000;
+                return Kuid;
             }
         }
 
 
         public long GetCurrentTimestamp()
         {
+            //Default timestamp got 13 digit so if we can output random sorting uid in 0.1 sec for 5 loop. 
+            //But timestamp is below than 13 output can be ascending uid in 0.1 sec for 5 loop. 
+            //loop can be ur option.
+
+            //For 13 digit timestamp.
             return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+            //For 9 digit timestamp.
+            //return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() / 10000;
         }
 
         public long WaitNextmillis(long lastTimestamp)
